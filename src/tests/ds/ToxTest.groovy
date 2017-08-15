f0bded42ae3ff03e3d5d9bb6e56ce945ab83d19e
hborcher <hborcher@illinois.edu>
2017-08-15 14:00:19 -0500

    implemented tox class test and created a mock runner

--- src/tests/ds/ToxTest.groovy
+++ src/tests/ds/ToxTest.groovy
@@ -1,9 +1,55 @@
 package tests.ds
+
 import org.ds.Tox
+
+
+class MockScript {
+    void sh(cmd) {}
+
+    void bat(cmd) {}
+    def node(LinkedHashMap args, Closure body){
+        return body()
+    }
+    def deleteDir() {}
+    def unstash(str){}
+}
+
 class ToxTest extends GroovyTestCase {
+    private runner
+
+    void setUp() {
+
+        runner = new Tox(new MockScript(), "/usr/bin/tox")
+    }
+
     void testInvokeMethod() {
-        def runner = new Tox("/usr/bin/tox")
         assert runner.toxPath == "/usr/bin/tox"
 
+    }
+
+    void testEnvAdder() {
+        runner.env = "pytest"
+        assert runner.env == "pytest"
+    }
+
+    void testDefaults() {
+        assert !runner.windows
+    }
+
+    void testRunWithPost() {
+
+
+        runner.post = { println("I'm running") }
+        runner.run()
+    }
+
+    void testRunWithoutPost() {
+        runner.run()
+    }
+
+    void testBuildCommand(){
+        runner.env = "pytest"
+        runner.windows = false
+        assert runner.buildToxCommand() == "/usr/bin/tox -e pytest"
     }
 }
