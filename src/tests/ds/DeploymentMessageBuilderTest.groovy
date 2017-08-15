226769e21f8fac276ea5910709076382df909c62
Henry Borchers <henryborchers@yahoo.com>
2017-08-15 16:14:22 -0500

    use mock

--- src/tests/ds/DeploymentMessageBuilderTest.groovy
+++ src/tests/ds/DeploymentMessageBuilderTest.groovy
@@ -1,13 +1,14 @@
 package tests.ds
 import org.ds.DeploymentMessageBuilder
 
+
 class DeploymentMessageBuilderTest extends GroovyTestCase {
     private builder
     void setUp(){
-        builder = new DeploymentMessageBuilder("d")
+        builder = new DeploymentMessageBuilder(new MockScript(),"message.yml")
     }
     void testInvokeMethod() {
-        assert builder.yaml == "d"
+        assert builder.yaml == "message.yml"
 
     }
 }
