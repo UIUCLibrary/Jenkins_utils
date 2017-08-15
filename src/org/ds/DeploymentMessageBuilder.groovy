2dc522ae3cf1480b188ceb5b621b3b465e739db8
Henry Borchers <henryborchers@yahoo.com>
2017-08-15 16:45:21 -0500

    explisit

--- src/org/ds/DeploymentMessageBuilder.groovy
+++ src/org/ds/DeploymentMessageBuilder.groovy
@@ -15,7 +15,7 @@ class DeploymentMessageBuilder {
 
         }
         script.echo "Reading yaml"
-        def config = script.readfile "deployment.yml"
+        def config = script.readFile "deployment.yml"
         script.echo "config = ${config}"
 //        def config = script.readYaml file: "deployment.yml"
 //        def config = script.readYaml file: "${yaml}"
