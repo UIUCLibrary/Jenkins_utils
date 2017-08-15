e17a85ffcdb948933ea4eea03b1290d705e688f7
Henry Borchers <henryborchers@yahoo.com>
2017-08-15 16:09:22 -0500

    use DeploymentMessageBuilder

--- vars/requestDeploy.groovy
+++ vars/requestDeploy.groovy
@@ -1,5 +1,8 @@
+import org.ds.DeploymentMessageBuilder
 def call(yaml){
     println("Creating deploy request message")
-    def deployment = readYaml file: "${yaml}"
-    echo "deployment = ${deployment}"
+    def builder = new DeploymentMessageBuilder(yaml)
+//    def deployment = readYaml file: "${yaml}"
+    def message = builder.build()
+    echo "yaml file is = ${yaml}"
 }
