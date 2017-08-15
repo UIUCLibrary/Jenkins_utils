2637cdb7747d900fd2d2c0aa521c4812b5e51ea9
Henry Borchers <henryborchers@yahoo.com>
2017-08-15 16:25:51 -0500

    added validation file

--- vars/requestDeploy.groovy
+++ vars/requestDeploy.groovy
@@ -2,6 +2,11 @@ import org.ds.DeploymentMessageBuilder
 def call(script, yaml){
     println("Creating deploy request message")
     echo "yaml file is = ${yaml}"
+    if (!fileExists(yaml)){
+        throw new FileNotFoundException("Unable to find file ${yaml}")
+
+    }
+
     def builder = new DeploymentMessageBuilder(script, yaml)
 //    def deployment = readYaml file: "${yaml}"
     def message = builder.build()
