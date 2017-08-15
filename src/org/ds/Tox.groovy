67ff30986fe4e3a08bcb31afd87bf6605d70178c
hborcher <hborcher@illinois.edu>
2017-08-15 13:59:58 -0500

    implemented tox class

--- src/org/ds/Tox.groovy
+++ src/org/ds/Tox.groovy
@@ -1,6 +1,39 @@
 package org.ds
 
-class Tox implements Serializable{
+class Tox implements Serializable {
     def toxPath
-    Tox(toxPath){this.toxPath = toxPath}
+    def script
+    def env
+    def label
+    def stash
+    def post = {}
+    def windows = false
+
+    Tox(script, toxPath) {
+        this.script = script
+        this.toxPath = toxPath
+    }
+
+    private def executeTox() {
+
+        if (windows) {
+            script.bat buildToxCommand()
+        } else {
+            script.sh buildToxCommand()
+        }
+
+    }
+
+    private def buildToxCommand() {
+        "${toxPath} -e ${env}"
+    }
+
+    def run() {
+        script.node(label: "${label}") {
+            script.deleteDir()
+            script.unstash "${stash}"
+            executeTox()
+            post()
+        }
+    }
 }
