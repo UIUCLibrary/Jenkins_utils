444101585ca05dbea75f059f96c913e802a4f047
Henry Borchers <henryborchers@yahoo.com>
2017-08-15 16:27:54 -0500

    added path validation

--- vars/requestDeploy.groovy
+++ vars/requestDeploy.groovy
@@ -1,11 +1,11 @@
 import org.ds.DeploymentMessageBuilder
 def call(script, yaml){
-    println("Creating deploy request message")
-    echo "yaml file is = ${yaml}"
     if (!fileExists(yaml)){
         throw new FileNotFoundException("Unable to find file ${yaml}")
 
     }
+    println("Creating deploy request message")
+    echo "yaml file is = ${yaml}"
 
     def builder = new DeploymentMessageBuilder(script, yaml)
 //    def deployment = readYaml file: "${yaml}"
