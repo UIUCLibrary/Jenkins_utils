a2d6faee3eda63b7186d079c32fa4f374834d27e
Henry Borchers <henryborchers@yahoo.com>
2017-08-15 16:23:30 -0500

    debug message

--- vars/requestDeploy.groovy
+++ vars/requestDeploy.groovy
@@ -1,8 +1,8 @@
 import org.ds.DeploymentMessageBuilder
 def call(script, yaml){
     println("Creating deploy request message")
+    echo "yaml file is = ${yaml}"
     def builder = new DeploymentMessageBuilder(script, yaml)
 //    def deployment = readYaml file: "${yaml}"
     def message = builder.build()
-    echo "yaml file is = ${yaml}"
 }
