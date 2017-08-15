2ef06cca810e6c9457915e909f5557caf8b4699d
hborcher <hborcher@illinois.edu>
2017-08-15 10:59:45 -0500

    rewrote to avoid requiring to be inside a script block

--- vars/tox.groovy
+++ vars/tox.groovy
@@ -1,9 +1,19 @@
-def call(body) {
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-    node {
-        println("Inside")
+//def call(body) {
+//    def config = [:]
+//    body.resolveStrategy = Closure.DELEGATE_FIRST
+//    body.delegate = config
+//    body()
+//    node {
+//        println("Inside")
+//    }
+//}
+
+def call(toxPath, env, stash, label, post){
+    node(label: "${label}"){
+        deleteDir()
+        unstash "${stash}"
+        sh "${toxPath} -e ${env}"
+        post()
     }
+
 }
