80eda215c7aa2d9ce7736e5bea5beeafff3495c0
hborcher <hborcher@illinois.edu>
2017-08-15 11:34:24 -0500

    using DSL style

--- vars/tox.groovy
+++ vars/tox.groovy
@@ -8,16 +8,25 @@
 //    }
 //}
 
-def call(toxPath, env, stash, label, post={}, windows=false){
-    node(label: "${label}"){
-        deleteDir()
-        unstash "${stash}"
-        if(windows){
-            bat "${toxPath} -e ${env}"
-        }else {
-            sh "${toxPath} -e ${env}"
+//def call(toxPath, env, stash, label, post={}, windows=false){
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    script {
+        node(label: "${config.label}") {
+            deleteDir()
+            unstash "${config.stash}"
+            if (config.windows) {
+                bat "${config.toxPath} -e ${config.env}"
+            } else {
+                sh "${config.toxPath} -e ${config.env}"
+            }
+            config.post()
         }
-        post()
+
     }
 
 }
