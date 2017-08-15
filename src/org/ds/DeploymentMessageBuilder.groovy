75b70a000fd656fd1c58811e1f0da0832733d464
Henry Borchers <henryborchers@yahoo.com>
2017-08-15 16:29:40 -0500

    debug info

--- src/org/ds/DeploymentMessageBuilder.groovy
+++ src/org/ds/DeploymentMessageBuilder.groovy
@@ -7,13 +7,15 @@ class DeploymentMessageBuilder {
         this.script = script
         this.yaml =yaml
     }
+
     def build(){
         if (!script.fileExists(this.yaml)){
             throw new FileNotFoundException("Unable to find file ${yaml}")
 
         }
-        def config = script.readYaml file: yaml
-        script.echo "config = ${config}"
+//        def config = script.readYaml file: yaml
+//        script.echo "config = ${config}"
+        script.sh "Reading something"
         return "message is not ready"
 
 
