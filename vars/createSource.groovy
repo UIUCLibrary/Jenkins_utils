eaaceb146640b101517e2220747fbecf36e8d892
hborcher <hborcher@illinois.edu>
2017-08-15 10:24:29 -0500

    turned on logic

--- vars/createSource.groovy
+++ vars/createSource.groovy
@@ -1,8 +1,11 @@
-def call(script, pythonPath, stashName, archive=true){
+def call(script, pythonPath, stashName, archive = true) {
     script.echo("Creating a source release for $stashName")
     script.echo("using $pythonPath. archive is set to $archive")
-//    deleteDir()
-//    unstash "Source"
-//    sh "${python_path} setup.py sdist"
-//    archiveArtifacts artifacts: "dist/**", fingerprint: true
+    node {
+        deleteDir()
+        unstash "$stashName"
+        sh "${python_path} setup.py sdist"
+        archiveArtifacts artifacts: "dist/**", fingerprint: archive
+    }
+
 }
