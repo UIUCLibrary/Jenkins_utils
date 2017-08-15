93a27d8beb1db009e3765f273b93147c10e35fd7
hborcher <hborcher@illinois.edu>
2017-08-15 10:14:17 -0500

    Added logic but commented out

--- vars/createSource.groovy
+++ vars/createSource.groovy
@@ -1,3 +1,7 @@
-def call(){
+def call(python_path){
     println("Creating a source release")
+//    deleteDir()
+//    unstash "Source"
+//    sh "${python_path} setup.py sdist"
+//    archiveArtifacts artifacts: "dist/**", fingerprint: true
 }
