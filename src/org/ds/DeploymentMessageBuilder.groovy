d39200cadcaaff163197c3f3d40e6bf419e67d24
Henry Borchers <henryborchers@yahoo.com>
2017-08-15 16:14:00 -0500

    added private script variable

--- src/org/ds/DeploymentMessageBuilder.groovy
+++ src/org/ds/DeploymentMessageBuilder.groovy
@@ -2,6 +2,7 @@ package org.ds
 
 class DeploymentMessageBuilder {
     private yaml
+    private script
     DeploymentMessageBuilder(script, yaml){
         this.script = script
         this.yaml =yaml
