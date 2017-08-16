661df5fd87dcda17762e74d54f66801ec4def789
Henry Borchers <henryborchers@yahoo.com>
2017-08-16 12:03:04 -0500

    got it working

--- src/tests/ds/DeploymentMessageBuilderTest.groovy
+++ src/tests/ds/DeploymentMessageBuilderTest.groovy
@@ -40,7 +40,33 @@ class DeploymentMessageBuilderTest extends GroovyTestCase {
         }
 
         service.use {
-            builder.build()
+            def expected_message = """Dear Scott,
+
+A new install package is ready for SCCM deployment.
+
+Filename: DS HathiTrust Checksum Updater-amd64.msi
+
+Location: \\\\files.library.illinois.edu\\groupfiles\\Digital Content Creation\\SCCM Upload\\
+
+
+The target hostnames:
+- LIBSTFDCC02
+- LIBSTFDCC03
+- LIBSTFDCC04
+- LIBSTFDCC06
+- LIBSTFDCC08
+- LIBSTFDCC09
+- LIBSTFDCC013
+
+The package has passed the msiexec silent install test.
+
+This is an automated message but if you have questions, please contact Henry Borchers at hborcher@illinois.edu.
+
+Thank you for your time.
+
+"""
+            def message = builder.build()
+            assert message == expected_message
         }
     }
 }
