69e98bf72c68b258c6e00daf422777687ff7e800
Henry Borchers <henryborchers@yahoo.com>
2017-08-16 11:05:36 -0500

    hostnames

--- src/org/ds/DeploymentConfigParser.groovy
+++ src/org/ds/DeploymentConfigParser.groovy
@@ -21,7 +21,7 @@ class DeploymentConfigParser implements Serializable {
         metadata.put("maintainer_email", yaml.maintainer.email)
 
         // deployment
-//        metadata.put("deployment_hostnames", yaml.package.path)
+        metadata.put("deployment_hostnames", yaml.deployment.hostnames)
         return metadata
     }
 }
