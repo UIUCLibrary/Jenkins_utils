11fcc41e70af2373fac134e83fa180335056cc3b
Henry Borchers <henryborchers@yahoo.com>
2017-08-28 17:31:16 -0500

    no longer in a node

--- vars/pythonSetup.groovy
+++ vars/pythonSetup.groovy
@@ -3,15 +3,13 @@ import org.ds.Setuptools
 def call(Map args) {
     def defaultArgs = [setup_script: "setup.py", windows: false]
     args = defaultArgs << args
-    node{
-        script{
-            sh "ls -la"
-            def installer = new Setuptools(this, "${args.python_path}")
-            installer.args = "${args.args}"
-            installer.setup_script = "${args.setup_script}"
-            installer.windows = args.windows
-            installer.run()
+    script{
+        sh "ls -la"
+        def installer = new Setuptools(this, "${args.python_path}")
+        installer.args = "${args.args}"
+        installer.setup_script = "${args.setup_script}"
+        installer.windows = args.windows
+        installer.run()
 
-        }
     }
 }
