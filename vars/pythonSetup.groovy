7a709c0c2d2e93cee04ecfd574d6ac41d852de2e
Henry Borchers <henryborchers@yahoo.com>
2017-08-28 17:18:54 -0500

    use node

--- vars/pythonSetup.groovy
+++ vars/pythonSetup.groovy
@@ -3,10 +3,13 @@ import org.ds.Setuptools
 def call(script, Map args) {
     def defaultArgs = [setup_script: "setup.py", windows: false]
     args = defaultArgs << args
-    echo "here I am"
-    def installer = new Setuptools(script, "${args.python_path}")
-    installer.args = "${args.args}"
-    installer.setup_script = "${args.setup_script}"
-    installer.windows = "${args.windows}"
-    installer.run()
+    node{
+        echo "here I am"
+
+        def installer = new Setuptools(script, "${args.python_path}")
+        installer.args = "${args.args}"
+        installer.setup_script = "${args.setup_script}"
+        installer.windows = "${args.windows}"
+        installer.run()
+    }
 }
