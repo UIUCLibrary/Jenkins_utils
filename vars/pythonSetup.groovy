3ae252bbac7b0a4103e331c805c18005ddc38503
Henry Borchers <henryborchers@yahoo.com>
2017-08-28 17:32:13 -0500

    removed debug information as it now works

--- vars/pythonSetup.groovy
+++ vars/pythonSetup.groovy
@@ -4,7 +4,6 @@ def call(Map args) {
     def defaultArgs = [setup_script: "setup.py", windows: false]
     args = defaultArgs << args
     script{
-        sh "ls -la"
         def installer = new Setuptools(this, "${args.python_path}")
         installer.args = "${args.args}"
         installer.setup_script = "${args.setup_script}"
