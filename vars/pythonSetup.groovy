5b659b8ab633570100687c5c91353db42b1f63b5
Henry Borchers <henryborchers@yahoo.com>
2017-08-28 17:17:57 -0500

    don't return runner

--- vars/pythonSetup.groovy
+++ vars/pythonSetup.groovy
@@ -1,12 +1,12 @@
 import org.ds.Setuptools
 
 def call(script, Map args) {
-    echo "here I am"
     def defaultArgs = [setup_script: "setup.py", windows: false]
     args = defaultArgs << args
+    echo "here I am"
     def installer = new Setuptools(script, "${args.python_path}")
     installer.args = "${args.args}"
     installer.setup_script = "${args.setup_script}"
     installer.windows = "${args.windows}"
-    return installer.run()
+    installer.run()
 }
