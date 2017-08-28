4be9add79774a579b07974062936085a16b1da5f
Henry Borchers <henryborchers@yahoo.com>
2017-08-28 17:20:02 -0500

    pushing inward

--- vars/pythonSetup.groovy
+++ vars/pythonSetup.groovy
@@ -1,12 +1,12 @@
 import org.ds.Setuptools
 
-def call(script, Map args) {
+def call(Map args) {
     def defaultArgs = [setup_script: "setup.py", windows: false]
     args = defaultArgs << args
     node{
         echo "here I am"
 
-        def installer = new Setuptools(script, "${args.python_path}")
+        def installer = new Setuptools(this, "${args.python_path}")
         installer.args = "${args.args}"
         installer.setup_script = "${args.setup_script}"
         installer.windows = "${args.windows}"
