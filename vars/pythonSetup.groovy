bd549e0ecc01b4daa309db754f7b28a0fa926588
Henry Borchers <henryborchers@yahoo.com>
2017-08-28 17:30:19 -0500

    debug

--- vars/pythonSetup.groovy
+++ vars/pythonSetup.groovy
@@ -5,6 +5,7 @@ def call(Map args) {
     args = defaultArgs << args
     node{
         script{
+            sh "ls -la"
             def installer = new Setuptools(this, "${args.python_path}")
             installer.args = "${args.args}"
             installer.setup_script = "${args.setup_script}"
