cbfd5ea07d6920553dfa449e989b2950f97ba45f
Henry Borchers <henryborchers@yahoo.com>
2017-08-29 11:05:24 -0500

    use class

--- vars/virtualenv.groovy
+++ vars/virtualenv.groovy
@@ -1,7 +1,13 @@
+import org.ds.VirtualEnv
 def call(Map args = [:], Closure body) {
     def python_path = args.get("python_path", "python")
     echo "python_path = ${python_path}"
     script {
+        def venv = new VirtualEnv(this, python_path)
+        venv.create_new()
+        venv.activate()
         body()
+        venv.deactivate()
+        venv.delete()
     }
 }
