ccb528958395a6f8328282383671539bccf38aa4
Henry Borchers <henryborchers@yahoo.com>
2017-08-29 12:39:42 -0500

    build with requirements if required

--- vars/virtualenv.groovy
+++ vars/virtualenv.groovy
@@ -3,7 +3,6 @@ import org.ds.VirtualEnv
 def call(Map args = [:], Closure body) {
     def python_path = args.get("python_path", "python")
     def windows = args.get("windows", false)
-    echo "python_path = ${python_path}"
     script {
         def venv = new VirtualEnv(this, python_path)
         venv.windows = windows
