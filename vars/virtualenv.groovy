8e550aeffca21d91f719508c0f4fdc2a533c28f7
Henry Borchers <henryborchers@yahoo.com>
2017-08-29 11:47:18 -0500

    pass windows arg

--- vars/virtualenv.groovy
+++ vars/virtualenv.groovy
@@ -1,13 +1,14 @@
 import org.ds.VirtualEnv
 def call(Map args = [:], Closure body) {
     def python_path = args.get("python_path", "python")
+    def windows = args.get("windows", false)
     echo "python_path = ${python_path}"
     script {
         def venv = new VirtualEnv(this, python_path)
+        venv.windows = windows
         venv.create_new()
         venv.runCommand("python --version")
         body()
-//        venv.deactivate()
         venv.delete()
     }
 }
